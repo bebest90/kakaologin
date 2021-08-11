f97037fea063457bdcd35c3caa454b6a7d6e2ac2
bebest90 <1990devops@gmail.com>
2021-08-11 13:14:58 +0900

    kakaologin complete

--- src/main/java/com/sparta/springcore/util/BeanConfiguration.java
+++ src/main/java/com/sparta/springcore/util/BeanConfiguration.java
@@ -1,4 +1,4 @@
-package com.sparta.springcore;
+package com.sparta.springcore.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
